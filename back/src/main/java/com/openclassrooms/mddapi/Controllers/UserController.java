fdd67ae9dc9efd3ea190b897c22526e77b6aef17
mdenizot <denizot.myriam@gmail.com>
2024-05-23 15:44:34 +0200

    Get user ID OK

--- back/src/main/java/com/openclassrooms/mddapi/Controllers/UserController.java
+++ back/src/main/java/com/openclassrooms/mddapi/Controllers/UserController.java
@@ -4,15 +4,23 @@ package com.openclassrooms.mddapi.Controllers;
 import com.openclassrooms.mddapi.Dtos.UserDto.UserDto;
 import com.openclassrooms.mddapi.Models.User;
 import com.openclassrooms.mddapi.Services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
 @RequestMapping("api")
 @RestController
 public class UserController
 {
+
+    @Autowired
     private UserService userService;
 
     /**
@@ -24,6 +32,17 @@ public class UserController
     @GetMapping("/me")
     public UserDto getCurrentUser(@AuthenticationPrincipal User user) {
         return userService.getCurrentUser(user);
+    }
+
+
+    @GetMapping("/user/{id}")
+    public ResponseEntity<UserDto> getUserById(@PathVariable Integer id) {
+        try {
+            UserDto userDto = userService.getUserById(id);
+            return ResponseEntity.ok(userDto);
+        } catch (NoSuchElementException e) {
+            return ResponseEntity.notFound().build();
+        }
     }
 
 }
--- back/src/main/java/com/openclassrooms/mddapi/Services/UserService.java
+++ back/src/main/java/com/openclassrooms/mddapi/Services/UserService.java
@@ -4,16 +4,24 @@ package com.openclassrooms.mddapi.Services;
 import com.openclassrooms.mddapi.Dtos.UserDto.UserDto;
 import com.openclassrooms.mddapi.Models.User;
 import com.openclassrooms.mddapi.Repositorys.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
 /**
  * Service pour la gestion des utilisateurs.
  */
 @Service
 public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public Optional<User> findById(Integer id) {
+        return userRepository.findById(id);
+    }
 
     /**
      * Récupère l'utilisateur courant.
@@ -22,4 +30,18 @@ public class UserService {
     public UserDto getCurrentUser(User user) {
         return new UserDto(user.getId(), user.getName(), user.getEmail(), user.getCreatedAt(), user.getUpdatedAt());
     }
+
+    public UserDto getUserById(Integer id) {
+        Optional<User> userOptional = userRepository.findById(id);
+        if (userOptional.isPresent()) {
+            User user = userOptional.get();
+            return getCurrentUser(user);
+        } else {
+            throw new NoSuchElementException("User not found with id: " + id);
+        }
+
+    }
+
+
+
 }
