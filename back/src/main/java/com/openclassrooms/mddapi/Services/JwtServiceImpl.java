b361935cc047d4bc0381e093ce4cd4b61126b8b5
WolfSilver0509 <denizot.myriam@gmail.com>
2024-08-01 16:10:17 +0200

    clean commentaires

--- back/src/main/java/com/openclassrooms/mddapi/Services/JwtServiceImpl.java
+++ back/src/main/java/com/openclassrooms/mddapi/Services/JwtServiceImpl.java
@@ -16,9 +16,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
-/**
- * Service pour la gestion des tokens JWT.
- */
+
 @Service
 public class JwtServiceImpl implements JwtService {
     @Value("${security.jwt.secret-key}")
@@ -27,72 +25,38 @@ public class JwtServiceImpl implements JwtService {
     @Value("${security.jwt.expiration-time}")
     private long jwtExpiration;
 
-    /**
-     * Extrait le nom d'utilisateur du token JWT.
-     *
-     * @param token Le token JWT à partir duquel extraire le nom d'utilisateur.
-     * @return Le nom d'utilisateur extrait du token JWT.
-     */
+  
     @Override
     public String extractUsername(String token) {
         return extractClaim(token, Claims::getSubject);
     }
 
-    /**
-     * Extrait une revendication spécifique du token JWT en utilisant un résolveur de revendications.
-     *
-     * @param token          Le token JWT à partir duquel extraire la revendication.
-     * @param claimsResolver Le résolveur de revendications pour extraire la revendication souhaitée.
-     * @param <T>            Le type de la revendication à extraire.
-     * @return La revendication extraite du token JWT.
-     */
+
     @Override
     public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
         final Claims claims = extractAllClaims(token);
         return claimsResolver.apply(claims);
     }
 
-    /**
-     * Génère un token JWT pour un utilisateur donné.
-     *
-     * @param userDetails Les détails de l'utilisateur pour lesquels générer le token JWT.
-     * @return Le token JWT généré.
-     */
+
     @Override
     public String generateToken(UserDetails userDetails) {
         return generateToken(new HashMap<>(), userDetails);
     }
 
-    /**
-     * Génère un token JWT avec des revendications supplémentaires pour un utilisateur donné.
-     *
-     * @param extraClaims Les revendications supplémentaires à inclure dans le token JWT.
-     * @param userDetails Les détails de l'utilisateur pour lesquels générer le token JWT.
-     * @return Le token JWT généré.
-     */
+
     @Override
     public String generateToken(Map<String, Object> extraClaims, UserDetails userDetails) {
         return buildToken(extraClaims, userDetails, jwtExpiration);
     }
 
-    /**
-     * Renvoie le temps d'expiration des tokens JWT.
-     *
-     * @return Le temps d'expiration des tokens JWT.
-     */
+
     @Override
     public long getExpirationTime() {
         return jwtExpiration;
     }
 
-    /**
-     * Construit un token JWT avec les revendications et le temps d'expiration spécifiés.
-     *
-     * @param extraClaims Les revendications supplémentaires à inclure dans le token JWT.
-     * @param userDetails Les détails de l'utilisateur pour lesquels générer le token JWT.
-     * @param expiration  Le temps d'expiration du token JWT.
-     * @return Le token JWT construit.
-     */
+
     @Override
     public String buildToken(Map<String, Object> extraClaims, UserDetails userDetails, long expiration) {
         return Jwts
@@ -105,47 +69,26 @@ public class JwtServiceImpl implements JwtService {
                 .compact();
     }
 
-    /**
-     * Vérifie si un token JWT est valide pour un utilisateur donné.
-     *
-     * @param token       Le token JWT à valider.
-     * @param userDetails Les détails de l'utilisateur pour lesquels valider le token JWT.
-     * @return true si le token JWT est valide, sinon false.
-     */
+ 
     @Override
     public boolean isTokenValid(String token, UserDetails userDetails) {
         final String username = extractUsername(token);
         return (username.equals(userDetails.getUsername())) && !isTokenExpired(token);
     }
 
-    /**
-     * Vérifie si un token JWT a expiré.
-     *
-     * @param token Le token JWT à vérifier.
-     * @return true si le token JWT a expiré, sinon false.
-     */
+
     @Override
     public boolean isTokenExpired(String token) {
         return extractExpiration(token).before(new Date());
     }
 
-    /**
-     * Extrait la date d'expiration d'un token JWT.
-     *
-     * @param token Le token JWT à partir duquel extraire la date d'expiration.
-     * @return La date d'expiration extraite du token JWT.
-     */
+
     @Override
     public Date extractExpiration(String token) {
         return extractClaim(token, Claims::getExpiration);
     }
 
-    /**
-     * Extrait toutes les revendications d'un token JWT.
-     *
-     * @param token Le token JWT à partir duquel extraire toutes les revendications.
-     * @return Toutes les revendications extraites du token JWT.
-     */
+
     @Override
     public Claims extractAllClaims(String token) {
         return Jwts
@@ -156,11 +99,6 @@ public class JwtServiceImpl implements JwtService {
                 .getBody();
     }
 
-    /**
-     * Récupère la clé de signature à partir de la clé secrète spécifiée.
-     *
-     * @return La clé de signature extraite de la clé secrète.
-     */
     @Override
     public Key getSignInKey() {
         byte[] keyBytes = Decoders.BASE64.decode(secretKey);
