79240a728630becea6e6c75da148b33824f3871a
Myriam Denizot <denizot.myriam@gmail.com>
2024-08-04 10:52:20 +0200

    add binsingresult , return dto , add global eception controle device

---
+++ back/src/main/java/com/openclassrooms/mddapi/Controllers/GlobalExceptionHandler.java
@@ -0,0 +1,35 @@
+package com.openclassrooms.mddapi.Controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    /*
+     * Gestion des exceptions de type MethodArgumentNotValidException.
+     */
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(error -> errors.put(error.getField(), error.getDefaultMessage()));
+        return ResponseEntity.badRequest().body(errors);
+    }
+
+    /*
+     * Gestion des exceptions de type RuntimeException.
+     */
+    @ExceptionHandler(RuntimeException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<String> handleRuntimeException(RuntimeException ex) {
+        return ResponseEntity.badRequest().body(ex.getMessage());
+    }
+}
--- back/src/main/java/com/openclassrooms/mddapi/Controllers/PostController.java
+++ back/src/main/java/com/openclassrooms/mddapi/Controllers/PostController.java
@@ -1,65 +1,64 @@
 package com.openclassrooms.mddapi.Controllers;
 
-
 import com.openclassrooms.mddapi.Dtos.PostDTO.PostDto;
 import com.openclassrooms.mddapi.Dtos.PostDTO.PostDtoResponseMessage;
 import com.openclassrooms.mddapi.Services.Interfaces.PostService;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 import java.security.Principal;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("api")
 public class PostController {
 
-        private final PostService postService;
+    private final PostService postService;
 
     public PostController(PostService postService) {
         this.postService = postService;
     }
 
     /*
-         * Point de terminaison pour créer un nouveau Post.
-         * Prend en entrée un DTO de création du commentaire et les informations de l'utilisateur.
-         * Retourne une réponse contenant le DTO de la réponse de création de Post.
-         */
-        @PostMapping("/posts")
-        public ResponseEntity<PostDtoResponseMessage> createPost(@ModelAttribute PostDto postDto, Principal principal) {
-            PostDtoResponseMessage responseMessage = postService.createPost(postDto, principal);
-            return ResponseEntity.status(HttpStatus.CREATED).body(responseMessage);
+     * Point de terminaison pour créer un nouveau Post.
+     * Prend en entrée un DTO de création du Post et les informations de l'utilisateur.
+     * Retourne une réponse contenant le DTO de la réponse de création de Post.
+     */
+    @PostMapping("/posts")
+    public PostDtoResponseMessage createPost(@Valid @ModelAttribute PostDto postDto, BindingResult bindingResult, Principal principal) {
+        if (bindingResult.hasErrors()) {
+            String errors = bindingResult.getFieldErrors().stream()
+                    .map(FieldError::getDefaultMessage)
+                    .collect(Collectors.joining(", "));
+            throw new RuntimeException("Validation errors: " + errors);
         }
+        return postService.createPost(postDto, principal);
+    }
 
-        /*
-         * Point de terminaison pour récupérer tous les Posts.
-         * Retourne une réponse contenant une liste de DTO PostDto.
-         */
+    /*
+     * Point de terminaison pour récupérer tous les Posts d'un utilisateur.
+     * Prend en entrée les informations de l'utilisateur.
+     * Retourne une liste de DTO PostDto.
+     */
     @GetMapping("/posts")
-    public ResponseEntity<List<PostDto>> getAllPosts(Principal principal) {
-        List<PostDto> posts = postService.getPostsByUser(principal);
-        return ResponseEntity.ok(posts);
+    public List<PostDto> getAllPosts(Principal principal) {
+        return postService.getPostsByUser(principal);
     }
 
     /*
      * Point de terminaison pour récupérer un Post par son ID.
      * Prend en entrée l'ID du Post.
-     * Retourne une réponse contenant le DTO du Post récupéré ou une réponse null si non trouvée.
+     * Retourne un DTO PostDto.
      */
     @GetMapping("/posts/{id}")
-    public ResponseEntity<PostDto> getPostById(@PathVariable Integer id, Principal principal) {
+    public PostDto getPostById(@PathVariable Integer id, Principal principal) {
         List<PostDto> userPosts = postService.getPostsByUser(principal);
-        PostDto postDto = userPosts.stream()
+        return userPosts.stream()
                 .filter(post -> post.getId() == id)
                 .findFirst()
-                .orElse(null);
-        if (postDto != null) {
-            return ResponseEntity.ok(postDto);
-        } else {
-            return ResponseEntity.notFound().build();
-        }
+                .orElseThrow(() -> new RuntimeException("Post not found"));
     }
-
-
-
 }
