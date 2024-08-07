5f5788e44ff958bc4a0174ddec51b8aa39a5ee68
Myriam Denizot <denizot.myriam@gmail.com>
2024-08-07 17:23:54 +0200

    add return topic when getme User

--- back/src/main/java/com/openclassrooms/mddapi/Repositorys/TopicRepository.java
+++ back/src/main/java/com/openclassrooms/mddapi/Repositorys/TopicRepository.java
@@ -4,11 +4,13 @@ import com.openclassrooms.mddapi.Models.Topic;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 /**
  * Interface de dépot pour la gestion des thémes dans la base de données .
  *
  */
 @Repository
 public interface TopicRepository extends JpaRepository<Topic, Integer>  {
-
+    List<Topic> findByUsers_Id(Integer userId);
 }
